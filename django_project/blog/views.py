9b544ca235973e41b3386f76e6ea8da14ccdc46c
Ahmad Mirzaei <ahmad.mirzaei@zoodfood.com>
2020-01-22 20:01:43 +0330

    upd

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-
-
+  
+posts = [
+  {
+    'author': 'User_01',
+    'title': 'this is tile one',
+    'content': 'loremsaf sad fsad fsad f sd ',
+    'date_posted': 'August 27, 2018'
+  },
+  {
+    'author': 'User_02',
+    'title': 'this is tile Two',
+    'content': 'loremsaf sad faef aessad fsad f sd ',
+    'date_posted': 'August 28, 2018'
+  }
+]
 
 def home(request):
-  return render(request, 'blog/home.html' )
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 
 def about(request):
