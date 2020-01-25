afc9c1961d657545f2c4dccd05b5f67324479a71
Ahmad Mirzaei <ahmad.mirzaei@zoodfood.com>
2020-01-25 20:16:47 +0330

    5 done

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
+from .models import Post
+
   
-posts = [
-  {
-    'author': 'User_01',
-    'title': 'this is tile one',
-    'content': 'loremsaf sad fsad fsad f sd ',
-    'date_posted': 'August 27, 2018'
-  },
-  {
-    'author': 'User_02',
-    'title': 'this is tile Two',
-    'content': 'loremsaf sad faef aessad fsad f sd ',
-    'date_posted': 'August 28, 2018'
-  }
-]
+# posts = [
+#   {
+#     'author': 'User_01',
+#     'title': 'this is tile one',
+#     'content': 'loremsaf sad fsad fsad f sd ',
+#     'date_posted': 'August 27, 2018'
+#   },
+#   {
+#     'author': 'User_02',
+#     'title': 'this is tile Two',
+#     'content': 'loremsaf sad faef aessad fsad f sd ',
+#     'date_posted': 'August 28, 2018'
+#   }
+# ]
 
 def home(request):
+  posts = Post.objects.all()
   context = {
     'title': 'Home',
     'posts': posts,
